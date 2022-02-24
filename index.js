6af78741b936cb8737243a1ac193d4f7d7577489
Queiroz <kalifor_dbel@hotmail.com>
2022-02-24 18:40:19 -0300

    Implementiing views, functions and libs.

--- index.js
+++ index.js
@@ -2,14 +2,28 @@
 const express = require("express");
 
 const app = express();//Instance of express
-//Render HTML 
-app.set("view engine", "ejs");
+const bodyParser = require("body-parser");
 
+//Render HTML with EJS
+app.set("view engine", "ejs");
 app.use(express.static('public'));
 
+//Using Body-Parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
+//Routes
 app.get("/", (request, response) => {
   response.render("index");
 });
 
+app.get("/ask", (request, response)=>{
+response.render("ask");
+});
+
+app.post("/saveask", (request, response)=>{
+var title = request.body.title;
+var desc = request.body.description;
+response.send("Form received as:" + title + " " + desc);
+});
 app.listen(1700);
