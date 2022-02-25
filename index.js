f3492c49ce2f031eec6e9065e02b44d99a2c5c51
Queiroz <kalifor_dbel@hotmail.com>
2022-02-25 00:00:32 -0300

    Database and Model implemented.

--- index.js
+++ index.js
@@ -1,29 +1,51 @@
 //Create an basic structure using and importing express's module
 const express = require("express");
-
 const app = express();//Instance of express
 const bodyParser = require("body-parser");
+const connection = require("./database/database");
+const Asking = require("./database/Asking");
+//Using Body-Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+//Database
+connection.authenticate().then(() => {
+  console.log("Connection succefully")
+}).catch((msgErro) => {
+  console.log(msgErro);
+})
 //Render HTML with EJS
 app.set("view engine", "ejs");
 app.use(express.static('public'));
 
-//Using Body-Parser
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
 
 //Routes
 app.get("/", (request, response) => {
-  response.render("index");
+  Asking.findAll({
+    raw: true, order: [
+      ['id', 'DESC']
+    ]
+  }).then(asks => {
+    response.render("index", {
+      asks: asks
+    });
+  });
 });
 
-app.get("/ask", (request, response)=>{
-response.render("ask");
+app.get("/ask", (request, response) => {
+  response.render("ask");
 });
 
-app.post("/saveask", (request, response)=>{
-var title = request.body.title;
-var desc = request.body.description;
-response.send("Form received as:" + title + " " + desc);
+app.post("/saveask", (request, response) => {
+
+  var title = request.body.title;
+  var description = request.body.description;
+
+  Asking.create({
+    title: title,
+    description: description
+  }).then(() => {
+    response.redirect("/");
+  });
 });
 app.listen(1700);
